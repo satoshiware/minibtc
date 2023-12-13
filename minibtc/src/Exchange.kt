a0e24deae1639a143f73ee4fd913877c449038cc
satoshiware <satoshiware@gmail.com>
2023-12-13 11:13:02 -0700

    Add Kotlin Project

---
+++ minibtc/src/Exchange.kt
@@ -0,0 +1,42 @@
+// Could we simply pass it the handle to the server with credentials and stuff already prepped????
+// Not sure? Program a bugger or something.
+
+class Exchange {
+    private var minimumOrder: Int = 0
+    private var makerFee: Int = 0
+    private var takerFee: Int = 0
+
+ //   private val buyOrders
+//    private val sellOrders
+    //private val trades
+
+
+    init {
+        // So, each exchange has an order book (buy, sell)
+        // Each exchange has a Trade book.
+
+        minimumOrder
+    }
+
+    // What is the presicion?
+
+    fun buy(id: Int, amount: Long, price: Long) {
+//        isOn = false
+    }
+
+    fun sell(id: Int, amount: Long, price: Long) {
+        //isOn = false
+        // It could return the ID? How much was executed etc.
+    }
+}
+
+// Well, they are float, but we need presicion. Everytime!! what do we do here?
+
+
+//Bitcoin, for example
+        // UTF8 defult charcter set for mariaDB. User: root, password: MariaDB
+        // Servicee Name: MariaDB
+        // TCP Port: 3306
+        // Buffer pool size: 8155 MB
+        // Page size: 16 KB
+
