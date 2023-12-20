2e56ed9a3d2724888492b3d7f7db569df71a3677
satoshiware <satoshiware@gmail.com>
2023-12-20 14:48:06 -0700

    Update OrderBook.kt

--- src/OrderBook.kt
+++ src/OrderBook.kt
@@ -3,7 +3,7 @@
 
 class OrderBook() {
     val BUY: Boolean = true;
-    val SELL: Boolean = true;
+    val SELL: Boolean = false;
 
     var buyOrders: Order? = null; private set
     var sellOrders: Order? = null; private set
@@ -16,20 +16,34 @@ class OrderBook() {
         sellQTY = 0
     }
 
-    fun add(order: Order, type: Boolean) {
-        if(type == BUY) {
+    fun add(order: Order) {
+        if(order.type == BUY) {
             try {
-                buyOrders = buyOrders!!.insert(order)
+                buyOrders!!.insert(order)
+                if(buyOrders!!.right === order)
+                    buyOrders = order
             } catch (e: NullPointerException) {
                 buyOrders = order
             }
         } else {
             try {
-                sellOrders = sellOrders!!.insert(order)
+                sellOrders!!.insert(order)
+                if(sellOrders!!.left === order)
+                    sellOrders = order
             } catch (e: NullPointerException) {
                 sellOrders = order
             }
         }
+    }
+
+    fun slippage(type: Boolean, amount: Long) {
+        if(type == BUY) {
+            //buyOrders.amount
+        } else {
+
+        }
+
+
     }
 
     // Remove an order
@@ -55,22 +69,22 @@ class OrderBook() {
 
 
 
-    class Order(val orderID: Long, val userID: Long, val price: Long, amount: Long) {
+    class Order(val orderID: Long, val userID: Long, val type: Boolean, val price: Long, amount: Long) {
         var amount: Long; private set
-        private var left: Order? = null
-        private var right: Order? = null
-        var child: Order? = null; private set
+        internal var left: Order? = null; private set
+        internal var right: Order? = null; private set
+        var offspring: Order? = null; private set
 
         init {
             this.amount = amount
         }
 
         internal fun insert(order: Order) {
-            if (order.price > this.price) {
+            if (type.xor(order.price > this.price)) {
                 if (this.right == null) {
                     this.right = order
                     order.left = this
-                } else if (order.price < this.right!!.price) {
+                } else if (type.xor(order.price < this.right!!.price)) {
                     order.right = this.right
                     this.right = order
                     order.left = this
@@ -78,11 +92,11 @@ class OrderBook() {
                 } else {
                     this.right!!.insert(order)
                 }
-            } else if (order.price < this.price) {
+            } else if (type.xor(order.price < this.price)) {
                 if (this.left == null) {
                     this.left = order
                     order.right = this
-                } else if (order.price > this.left!!.price) {
+                } else if (type.xor(order.price > this.left!!.price)) {
                     order.left = this.left
                     this.left = order
                     order.right = this
@@ -92,16 +106,16 @@ class OrderBook() {
                 }
             } else {
                 this.amount += order.amount
-                if (this.child == null) {
-                    this.child = order
+                if (this.offspring == null) {
+                    this.offspring = order
                 } else {
-                    this.child!!.insert(order)
+                    this.offspring!!.insert(order)
                 }
             }
         }
 
         internal fun remove() {
-            if (this.child == null) {
+            if (this.offspring == null) {
                 if (this.left != null) {
                     this.left!!.right = this.right
                 }
@@ -109,10 +123,10 @@ class OrderBook() {
                     this.right!!.left = this.left
                 }
             } else {
-                this.child!!.left = this.left
-                this.left?.right = this.child
-                this.child!!.right = this.right
-                this.right?.left = this.child
+                this.offspring!!.left = this.left
+                this.left?.right = this.offspring
+                this.offspring!!.right = this.right
+                this.right?.left = this.offspring
             }
         }
 
@@ -121,13 +135,46 @@ class OrderBook() {
 //        this.price < price -> right?.findByPrice(price)
 //        else -> this
 //    }
-        private fun find(orderID: Long): Order? {
+
+        internal fun find(orderID: Long): Order? {
             if (this.orderID == orderID)
                 return this
-            else if (this.right == null)
+            else if (if (this.type) this.left == null else this.right == null)
                 return null
             else
-                return this.right!!.find(orderID)
+                return if (this.type) this.left!!.find(orderID) else this.right!!.find(orderID)
+        }
+
+        internal fun slippage(amount: Long): Long {
+            if (this.type) {
+                if (amount > this.amount)
+                    if (this.right != null)
+                        return this.right?.slippage(price)
+                    else
+                        return
+                else
+                    return this.price
+            } else {
+                if (amount > this.amount)
+                    if (this.right != null)
+                        return this.right?.slippage(price)
+                    else
+                        return
+                else
+                    return this.price
+
+
+            }
+
+
+            this
+
+            if (this.orderID == orderID)
+                return this
+            else if (if (this.type) this.left == null else this.right == null)
+                return null
+            else
+                return if (this.type) this.left!!.find(orderID) else this.right!!.find(orderID)
         }
     }
 }
